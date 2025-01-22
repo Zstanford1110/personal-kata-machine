4b209610eebc3cc6e6522e763184ba7f139f17e1
Zachary Stanford <zachary.s1110@gmail.com>
2025-01-21 23:52:09 -0500

    Implement Linear and Binary Searches

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let lo = 0;
+  let hi = haystack.length;
 
+
+  while(lo < hi) {
+    const mid = Math.floor(lo + (hi - lo) / 2);
+    const currentValue = haystack[mid];
+
+    if(currentValue === needle)
+      return true;
+
+    if(currentValue < needle)
+      lo = mid + 1; // Inclusive of lo, don't check the mid value twice
+
+    if(currentValue > needle)
+      hi = mid; // Exclusive of hi
+  }
+
+  return false;
 }
